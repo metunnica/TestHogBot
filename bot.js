2c7c35e2dd22c3fef17d4fe74c8428a3290f3770
Bill Scott <Metunnica@gmail.com>
2018-04-16 16:42:40 +0100

    removed retweet funtion

--- bot.js
+++ bot.js
@@ -144,36 +144,3 @@ Twitter.stream('statuses/filter', {track: '#HogibMayMovies'}, function(stream) {
         console.log(error);
     });
 });
-
-//Retweet #Hogibs
-var retweet = function(){
-    var params = {
-        q: '#Hogibtest, #hogibtest',
-        result_type: 'recent'
-    }
-    Twitter.get('search/tweets', params, function(err, data) {
-        if (!err) {
-            var retweetId = data.statuses[0].id_str;
-            Twitter.post ('statuses/retweet/:id', {
-                id: retweetId
-            }, function(err, responce) {
-                if (responce) {
-                    console.log('Retweeted!!!');
-                }
-                if (err) {
-                    console.log('Somthing went wrong while retweeting. Dope...');
-                }
-            });
-        }
-        else {
-            console.log('Somthing went wrong while searching...')
-        }
-    });
-}
-//loop
-setInterval(function() {
-
-    retweet();
-    console.log('loop restarted')
-
-   }, 5000);
